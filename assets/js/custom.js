64da0bdbfbc4fde18f3c52400924cfbe2eb3fa46
xytbruh <127030192+xytbruh@users.noreply.github.com>
2023-05-29 13:06:27 +0700

    edit

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -56,5 +56,13 @@
         );
     });
   });
- 
+  const stickyNavbar = document.querySelector("nav.sticky-navbar");
+  
+  document.addEventListener("scroll", () => {
+    if (window.scrollY > 36) {
+      stickyNavbar.classList.add("scrolling");
+    } else {
+      stickyNavbar.classList.remove("scrolling");
+    }
+  });
 })(window.jQuery);
